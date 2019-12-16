a2baf47888eaec6d84294e2dc4d31b80e1e29db9
Henri Loberg <51534125+WoodCharp@users.noreply.github.com>
2019-12-16 13:24:07 +0200

    Add files via upload

---
+++ PulserHandwheel/PulserHandwheel.ino
@@ -0,0 +1,48 @@
+const byte A = 2;
+volatile byte A_State;
+const byte B = 3;
+volatile byte B_State;
+int pulserSteps = 0;
+int dir;
+
+bool aRead = false, bRead = false;
+
+void setup() {
+  pinMode(A, INPUT_PULLUP);
+  pinMode(B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(A), ReadPulserA, FALLING);
+  attachInterrupt(digitalPinToInterrupt(B), ReadPulserB, FALLING);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if(aRead && bRead)
+  {
+    aRead = false;
+    bRead = false;
+
+    switch(dir)
+    {
+      case 1:
+        pulserSteps--;
+        break;
+      case 2:
+        pulserSteps++;
+        break;
+    }
+     
+    Serial.println(pulserSteps);
+  }
+}
+
+void ReadPulserA()
+{
+  dir = 1;
+  aRead = true;
+}
+
+void ReadPulserB()
+{
+  dir = 2;
+  bRead = true;
+}
